578555f9a9847ed622d9acd8a5dcb4a093614f47
Thurman <XUPD48RET@Whaynenet.com>
2019-08-26 10:29:44 -0400

    added the datepicker component
    Added the component, probably will not implement until MVP is reached.

---
+++ src/components/Header/Datepicker.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import the datepicker dependancy
+import DatePicker from 'react-datepicker';
+
+// datepicker styles
+import 'react-datepicker/dist/react-datepicker.css';
+
+const Date = () => {
+	// Hook might need moved into the app component
+	const [ startDate, setStartDate ] = useState(new Date());
+
+	return (
+		<DatePicker
+			dateFormat='yyyy/MM/dd'
+			todayButton='Today'
+			selected={startDate}
+			onChange={(date) => setStartDate(date)}
+		/>
+	);
+};
+
+export default Date;
