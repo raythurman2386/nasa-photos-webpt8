ea1892c041289309e57cb8d1498a54feab205333
raythurman2386 <raymondthurman5@gmail.com>
2019-08-26 21:51:10 -0400

    description is rendering to the page

--- src/components/PhotoContent/Description.js
+++ src/components/PhotoContent/Description.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Description = ({ data }) => {
-  return <div></div>;
+  return (
+    <div>
+      <p>{data.explanation}</p>
+    </div>
+  );
 };
 
 export default Description;
