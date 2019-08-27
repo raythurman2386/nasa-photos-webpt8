0b3b12107518e8a4d03dda33e742ecdeb9087770
raythurman2386 <raymondthurman5@gmail.com>
2019-08-26 21:50:31 -0400

    image is rendering to the page

--- src/components/PhotoContent/Image.js
+++ src/components/PhotoContent/Image.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Image = ({ data }) => {
-  return <div className='image-container'></div>;
+  return (
+    <div className='image-container'>
+      <img src={data.url} alt={data.title} />
+    </div>
+  );
 };
 
 export default Image;
